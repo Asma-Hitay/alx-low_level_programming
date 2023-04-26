613820aa1182945cb5a8585df02c10aa23f78ca8
Asma-Hitay <asmavict@gmail.com>
2023-04-26 16:54:47 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+1 #include "main.h"
+2 /**
+3 * print_alphabet - Prints the alphabet in lowercase.
+4 *
+5 * Return: Always 0 (Saccess)
+6 */
+7 void print_alphabet(void)
+8 {
+9      int i;
+10
+11     for (i= 97; i<=122; i++)
+12       {
+13	_putchar(i);
+14       }
+15      _putchar('\n');
+16
+
